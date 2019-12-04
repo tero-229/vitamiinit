cf75dbd65e279d86032323f856c551f8e79ae538
tiinaelisa <heikkinentiinaelisa@gmail.com>
2019-12-04 11:37:54 +0200

    started handler.py

--- handler.py
+++ handler.py
@@ -1,24 +1,54 @@
 import json
+import boto3
+import os
 
+region_name = 'eu-central-1'    
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+#curlataan GETillä 'kalium', joka palauttaa finelin taulukosta DESCRIPTIONin kaliumille
+#lähetetään curlattu RESPONSE emailina
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+tulos = "Valitettavasti haullasi ei löytynyt yhtään tulosta"
 
-    return response
+def getDescription(event, context):
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
+    data = requests.get('https://fineli.fi/fineli/api/v1/components', headers=headers).json()
+
+    search_criteria = event['search']
+
+    for item in data:
+                
+        description = item['description']['fi']
+        if search_criteria in description.casefold():
+            tulos = description
+           
+
+def sendEmail(event, context):
+    data = event['body']
+    name = data['name'] 
+    destination = data['destination']
+    source = data['source']
+    subject = data['subject']
+    _message = "Message from: " + name + "\nEmail: " + source + "\nMessage content: " + tulos 
+    
+    client = boto3.client('ses' )    
+        
+    response = client.send_email(
+        Destination={
+            'ToAddresses': [destination]
+            },
+        Message={
+            'Body': {
+                'Text': {
+                    'Charset': 'UTF-8',
+                    'Data': _message,
+                },
+            },
+            'Subject': {
+                'Charset': 'UTF-8',
+                'Data': subject,
+            },
+        },
+        Source=source,
+    )
+    return _message + str(region_name)
