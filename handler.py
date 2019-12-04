f722d4e5b00c0ecf311195a893b388253991730e
Tero <tero.makila@hotmail.com>
2019-12-04 13:03:17 +0200

    handleria muokattu

--- handler.py
+++ handler.py
@@ -1,34 +1,40 @@
 import json
 import boto3
 import os
+from botocore.vendored import requests
 
-region_name = 'eu-central-1'    
+region_name = os.environ['REGION_NAME']   
 
 #curlataan GETillä 'kalium', joka palauttaa finelin taulukosta DESCRIPTIONin kaliumille
 #lähetetään curlattu RESPONSE emailina
 
-tulos = "Valitettavasti haullasi ei löytynyt yhtään tulosta"
 
-def getDescription(event, context):
+
+def getDescription(search_criteria):
 
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'}
     data = requests.get('https://fineli.fi/fineli/api/v1/components', headers=headers).json()
 
-    search_criteria = event['search']
+    tulos = "Valitettavasti haullasi ei löytynyt yhtään tulosta"
 
     for item in data:
                 
         description = item['description']['fi']
         if search_criteria in description.casefold():
             tulos = description
-           
+        else:
+            tulos
+
+    return tulos
 
 def sendEmail(event, context):
     data = event['body']
     name = data['name'] 
     destination = data['destination']
+    search_criteria = event['search']
     source = data['source']
     subject = data['subject']
+    tulos = getDescription[search_criteria]
     _message = "Message from: " + name + "\nEmail: " + source + "\nMessage content: " + tulos 
     
     client = boto3.client('ses' )    
